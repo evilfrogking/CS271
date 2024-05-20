92f714c242eba3ea20053093d40f6cbb595fbba5
Aspen Frazee <aspen.frazee.3586@mail.linnbenton.edu>
2024-05-19 22:14:35 -0700

    Finished fill!
    Collab with Sam

--- project4/fill/Fill.asm
+++ project4/fill/Fill.asm
@@ -13,3 +13,79 @@
 
 // Put your code here.
 
+// Sets the end of the screen as a variable for readability
+@24575
+D=A
+@end
+M=D
+
+(LOOP)
+
+    @position
+    M=0
+
+    @KBD
+    D=M
+
+    @KBD_PRESSED
+    D;JNE
+
+    @NONE_PRESSED
+    D;JEQ
+
+(NONE_PRESSED)
+    //RESETS SCREEN BACK TO WHITE
+    @KBD
+    D=M
+    @LOOP
+    D;JNE // if a key is pressed during the loop, goes to loop
+
+    @SCREEN
+    D=A //GET ADRESS OF SCREEN
+
+    @position
+    D=D+M // GET CURRENT POSITION TO RESET
+
+    A=D //SET CURRENT POSITION
+    M=0 //RESET SCREEN AT CURRENT POSITION 
+
+    @end
+    D=D-M
+
+    @LOOP //IF AT LAST, LOOP BACK
+    D;JEQ
+
+    @position //UPDATE POSITION
+    M=M+1
+
+    @NONE_PRESSED //LOOP BACK FOR NEXT POSITION
+    0;JMP
+
+(KBD_PRESSED)
+    //SETS SCREEN TO BLACK
+    @KBD
+    D=M
+    @LOOP
+    D;JEQ // if a key is not pressed during the loop, goes to loop
+
+    @SCREEN
+    D=A //GET ADRESS OF SCREEN
+
+    @position
+    D=D+M //GET CURRENT POSITION TO FILL 
+
+    A=D //SET THE CURRENT POSITION'S ADDRESS TO EDIT 
+    M=-1 //FILL THE SCREEN AT CURRENT POSITION
+
+    @end
+    D=D-M
+
+    @LOOP //IF LAST AVAIABLE IS REACHED GO BACK TO INTIAL LOOP 
+    D;JEQ
+
+    @position //UPDATE POSITION
+    M=M+1
+
+    @KBD_PRESSED //LOOP BACK FOR NEXT POSITION 
+    0;JMP
+
---
+++ project4/fill/screen.asm
@@ -0,0 +1,7 @@
+//Screen test
+@i
+M=1
+(LOOP)
+    @SCREEN
+    M=-1
+
