1f8a8b1d8d1463723fe2460669b22b818c6e45a6
Aspen Frazee <aspen.frazee.3586@mail.linnbenton.edu>
2024-06-07 19:52:06 -0700

    comp table dict

--- project6/hack_assembler.py
+++ project6/hack_assembler.py
@@ -48,6 +48,48 @@ def parse(input_line: str) -> str:
 
     return output_line
 
+def init_comp_lookup_dict() -> dict:
+    """_summary_
+
+    Returns:
+        dict: _description_
+    """
+    # build comp look up dict as a comp instruction as the key and its control bits as the value
+    comp_lookup = {}
+    #A'S
+    comp_lookup["0"]   = "0101010"
+    comp_lookup["1"]   = "0111111"
+    comp_lookup["-1"]  = "0111010"
+    comp_lookup["D"]   = "0001100"
+    comp_lookup["A"]   = "0110000"
+    comp_lookup["!D"]  = "0001101"
+    comp_lookup["!A"]  = "0110001"
+    comp_lookup["-D"]  = "0001111"
+    comp_lookup["-A"]  = "0110011"
+    comp_lookup["D+1"] = "0011111"
+    comp_lookup["A+1"] = "0110111"
+    comp_lookup["D-1"] = "0001110"
+    comp_lookup["A-1"] = "0110010"
+    comp_lookup["D+A"] = "0000010"
+    comp_lookup["D-A"] = "0010011"
+    comp_lookup["A-D"] = "0000111"
+    comp_lookup["D&A"] = "0000000"
+    comp_lookup["D|A"] = "0010101"
+    # M'S
+    comp_lookup["M"]   = "1110000"
+    comp_lookup["!M"]  = "1110001"
+    comp_lookup["-M"]  = "1110011"
+    comp_lookup["M+1"] = "1110111"
+    comp_lookup["M-1"] = "1110010"
+    comp_lookup["D+M"] = "1000010"
+    comp_lookup["D-M"] = "1010011"
+    comp_lookup["M-D"] = "1000111"
+    comp_lookup["D&M"] = "1000000"
+    comp_lookup["D|M"] = "1010101"
+
+
+
+
 
 if __name__ == "__main__":
     main()
